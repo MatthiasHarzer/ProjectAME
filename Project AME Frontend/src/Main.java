a118ec8984dce694f961ec0c7068adc367cbe30a
Matthias Harzer <matthias.harzer03@gmail.com>
2021-01-27 17:28:32 +0100

    public

--- Project AME Frontend/src/Main.java
+++ Project AME Frontend/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	private static int i = 0;
-	private static void main(String args[]) {
-		System.out.println("");
+	public static void main(String[] args) {
+		System.out.println("asd");
 	}
 }

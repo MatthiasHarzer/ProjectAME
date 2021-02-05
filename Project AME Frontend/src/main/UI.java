bb05d74f84869a6534e085daab398b4780c27d39
AnoukTruog <anouktruogbuisness@gmail.com>
2021-02-05 16:19:52 +0100

    Provisorische Lösung der Ausgabe durch Konsole  uwu

--- Project AME Frontend/src/main/UI.java
+++ Project AME Frontend/src/main/UI.java
@@ -1,11 +1,31 @@
 package main;
 
-public class UI {
+import java.util.concurrent.TimeUnit;
 
+public class UI {
+ 
+	Communication com;
+	
 	private int i = 0;
 	
 	public UI() {
 		System.out.println("UI has been created");
+		
+	}
+	
+	public void setCommunication(Communication pcom) {
+		this.com = pcom;
+	}
+	
+	
+	
+	public void displayMessage(String content, String name, String time) {
+		
+		//insert ui stuff here
+		
+		//provisorische lösung
+ 	 System.out.println("it's " + time + " and " +name + " says:  " + content);
+		
 	}
 	
 }
